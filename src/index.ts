ed6ca3ff8e4a073282533e811711d2e5966f8b27
Dhruva Srinivas <dhruvas17068@gmail.com>
2022-04-27 09:04:39 +0530

    login

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,56 @@ import * as Express from "express";
 import { buildSchema } from "type-graphql";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import { createConnection } from "typeorm";
+import * as session from "express-session";
+import * as connectRedis from "connect-redis";
+import * as cors from "cors";
+
+// Resolvers
 import { RegisterResolver } from "./modules/user/Register";
+import { redis } from "./redis";
+import { LoginResolver } from "./modules/user/Login";
+import { MeResolver } from "./modules/user/Me";
 
 const main = async () => {
   await createConnection();
 
   const schema = await buildSchema({
-    resolvers: [RegisterResolver],
+    resolvers: [RegisterResolver, LoginResolver, MeResolver],
   });
 
   const apolloServer = new ApolloServer({
     schema,
+    context: ({ req }: any) => ({ req }),
     plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
   });
 
   const app = Express();
+
+  const RedisStore = connectRedis(session);
+
+  app.use(
+    cors({
+      credentials: true,
+      origin: `http://localhost:3000`,
+    })
+  );
+
+  const sessionOption: session.SessionOptions = {
+    store: new RedisStore({
+      client: redis,
+    }),
+    name: "qid",
+    secret: "btssucks",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 1000 * 60 * 60 * 24 * 7 * 365, // 7 years
+    },
+  };
+
+  app.use(session(sessionOption));
 
   await apolloServer.start();
   apolloServer.applyMiddleware({ app });
---
+++ src/modules/user/Me.ts
@@ -0,0 +1,17 @@
+import { Ctx, Query, Resolver } from "type-graphql";
+import { User } from "../../entity/User";
+import { MyContext } from "../../types/MyContext";
+
+@Resolver()
+export class MeResolver {
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() ctx: MyContext): Promise<User | undefined> {
+    // @ts-ignore
+    if (!ctx.req.session!.userId) {
+      return undefined;
+    }
+
+    // @ts-ignore
+    return User.findOne({ where: { id: ctx.req.session!.userId } });
+  }
+}
